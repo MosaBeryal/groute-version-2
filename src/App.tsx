f6368e5fad149fb888704c5c259113db697248c1
mosa beryal <beryalmosa@gmail.com>
2024-07-17 12:28:22 +0500

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import router from "./router";
+import { RouterProvider } from "react-router-dom";
+
+const App:React.FC = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    ),
+  },
+]);
+
+export default router
